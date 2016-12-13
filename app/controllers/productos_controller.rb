ffc560ce0e0189dc29194e39824b5f4c6d1a9d09
ealtamar <eduardo.altamar@pragma.com.co>
2016-12-13 09:17:26 -0500

    first commit

---
+++ app/controllers/productos_controller.rb
@@ -0,0 +1,20 @@
+class ProductosController < ApplicationController
+  before_filter :require_login
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Login::Application.config.secret_token = '718bc65aa49b6508771d359702aa2f1f842e9d89d8f36bed3c136ffc439de48586888c4b3bdc3d20d2e847d75c97a6c00c89311bb26de17d74c78d2d49731d44'
